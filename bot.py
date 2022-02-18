4b607d123ac0d3d3908e72be2898a1879f939232
itcosplay <azure_dream@mail.ru>
2022-02-18 14:19:24 +0300

    Tested detect_intent_func

--- bot.py
+++ bot.py
@@ -10,6 +10,8 @@ from telegram.ext import (
     Filters
 )
 from environs import Env
+# from google.cloud import storage
+from google.cloud import dialogflow
 
 
 logger = logging.getLogger(__file__)
@@ -27,6 +29,34 @@ def echo(update: Update, context: CallbackContext):
         chat_id=update.effective_chat.id,
         text=update.message.text
     )
+
+
+def detect_intent_texts(project_id, session_id, text, language_code):
+    env = Env()
+    env.read_env()
+
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(project_id, session_id)
+    # print("Session path: {}\n".format(session))
+
+    text_input = dialogflow.TextInput(text=text, language_code=language_code)
+
+    query_input = dialogflow.QueryInput(text=text_input)
+
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
+
+    print("=" * 20)
+    print("Query text: {}".format(response.query_result.query_text))
+    print(
+        "Detected intent: {} (confidence: {})\n".format(
+            response.query_result.intent.display_name,
+            response.query_result.intent_detection_confidence,
+        )
+    )
+    print("Fulfillment text: {}\n".format(response.query_result.fulfillment_text))
+
 
 
 def main():
@@ -51,8 +81,8 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
-
+    # main()
+    detect_intent_texts('recognize-speech-bot-lgeb', '11223344', 'Привет', 'ru')
 
 
 
