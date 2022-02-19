147c2d6145d1ef210cd2af9e4a5e2769858d43e0
itcosplay <azure_dream@mail.ru>
2022-02-19 19:09:22 +0300

    Setup DialogFlow

--- bot.py
+++ bot.py
@@ -10,11 +10,23 @@ from telegram.ext import (
     Filters
 )
 from environs import Env
-# from google.cloud import storage
 from google.cloud import dialogflow
 
 
 logger = logging.getLogger(__file__)
+
+
+def detect_intent_texts(project_id, session_id, text, language_code):
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(project_id, session_id)
+    text_input = dialogflow.TextInput(text=text, language_code=language_code)
+    query_input = dialogflow.QueryInput(text=text_input)
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
+    print(response.query_result.fulfillment_text)
+
+    return response.query_result.fulfillment_text
 
 
 def command_start(update: Update, context: CallbackContext):
@@ -25,48 +37,29 @@ def command_start(update: Update, context: CallbackContext):
 
 
 def echo(update: Update, context: CallbackContext):
+    user_id = update.effective_user.id
+    user_text = update.message.text
+
+    answer = detect_intent_texts(
+        'recognize-speech-bot-lgeb',
+        user_id,
+        user_text,
+        'ru'
+    )
     context.bot.send_message(
         chat_id=update.effective_chat.id,
-        text=update.message.text
+        text=answer
     )
-
-
-def detect_intent_texts(project_id, session_id, text, language_code):
-    env = Env()
-    env.read_env()
-
-    session_client = dialogflow.SessionsClient()
-    session = session_client.session_path(project_id, session_id)
-    # print("Session path: {}\n".format(session))
-
-    text_input = dialogflow.TextInput(text=text, language_code=language_code)
-
-    query_input = dialogflow.QueryInput(text=text_input)
-
-    response = session_client.detect_intent(
-        request={"session": session, "query_input": query_input}
-    )
-
-    print("=" * 20)
-    print("Query text: {}".format(response.query_result.query_text))
-    print(
-        "Detected intent: {} (confidence: {})\n".format(
-            response.query_result.intent.display_name,
-            response.query_result.intent_detection_confidence,
-        )
-    )
-    print("Fulfillment text: {}\n".format(response.query_result.fulfillment_text))
-
 
 
 def main():
+    env = Env()
+    env.read_env()
+
     logging.basicConfig(
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
         level=logging.INFO
     )
-
-    env = Env()
-    env.read_env()
 
     updater = Updater(os.environ['TG_BOT_TOKEN'])
     dispatcher = updater.dispatcher
@@ -81,8 +74,8 @@ def main():
 
 
 if __name__ == '__main__':
-    # main()
-    detect_intent_texts('recognize-speech-bot-lgeb', '11223344', 'Привет', 'ru')
+
+    main()
 
 
 
