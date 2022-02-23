9b4d29d04ff8501a44d5bcd674c1c6d6ff330578
itcosplay <azure_dream@mail.ru>
2022-02-23 20:03:08 +0300

    Almost finish training dilogflow

--- bot.py
+++ bot.py
@@ -1,5 +1,8 @@
 import os
 import logging
+import pprint
+import json
+import requests
 
 from telegram import Update
 from telegram.ext import (
@@ -11,9 +14,41 @@ from telegram.ext import (
 )
 from environs import Env
 from google.cloud import dialogflow
+from google import api_core
 
 
 logger = logging.getLogger(__file__)
+
+
+
+def create_intent(
+    project_id,
+    display_name,
+    training_phrases_parts,
+    message_texts
+):
+    intents_client = dialogflow.IntentsClient()
+
+    parent = dialogflow.AgentsClient.agent_path(project_id)
+    training_phrases = []
+    for training_phrases_part in training_phrases_parts:
+        part = dialogflow.Intent.TrainingPhrase.Part(text=training_phrases_part)
+        training_phrase = dialogflow.Intent.TrainingPhrase(parts=[part])
+        training_phrases.append(training_phrase)
+
+    text = dialogflow.Intent.Message.Text(text=message_texts)
+    message = dialogflow.Intent.Message(text=text)
+
+    intent = dialogflow.Intent(
+        display_name=display_name, training_phrases=training_phrases, messages=[message]
+    )
+
+    response = intents_client.create_intent(
+        request={"parent": parent, "intent": intent, 'language_code': 'ru'},
+        
+    )
+
+    print('Intent was created!')
 
 
 def detect_intent_texts(project_id, session_id, text, language_code):
@@ -74,9 +109,56 @@ def main():
 
 
 if __name__ == '__main__':
+    env = Env()
+    env.read_env()
 
-    main()
+    questions = [
+        "Как устроиться к вам на работу?",
+        "Как устроиться к вам?",
+        "Как работать у вас?",
+        "Хочу работать у вас",
+        "Возможно-ли устроиться к вам?",
+        "Можно-ли мне поработать у вас?",
+        "Хочу работать редактором у вас"
+    ]
 
+    # answer = 'Если вы забанены, вы нарушили правила нашего сообщества. При входе на сайт вы можете увидеть доказательства ваших нарушений и ссылку на нарушенное правило. Разбан не продаётся. Если вы ознакомились с правилами и доказательствами вашей вины и у вас всё ещё есть претензии — воспользуйтесь формой «Не виновен» под сообщением о бане.'
+    answer = 'zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz'
+    print([answer])
+    # try:
+    #     create_intent(
+    #         'recognize-speech-bot-lgeb',
+    #         display_name='Устройство на работу',
+    #         training_phrases_parts=questions,
+    #         message_texts=[answer]
+    #         # message_answer = [intents[intent]['answer']]
+    #     )
+    # except api_core.exceptions.InvalidArgument:
+    #     print('Invalid arg!')
+    
+
+    # https://stackoverflow.com/questions/52332247/permissiondenied-403-iam-permission-dialogflow-intents-list
+
+    # main()
+
+
+    # with open('training_phrases.json', 'r') as file:
+    #     training_phrases = json.load(file)
+
+    # for training_phrase in training_phrases:
+    #     print(training_phrase)
+    #     display_name = training_phrase
+    #     questions = training_phrases[training_phrase]['questions']
+    #     answer = training_phrases[training_phrase]['answer']
+
+    #     create_intent(
+    #         'recognize-speech-bot-lgeb',
+    #         display_name=display_name,
+    #         training_phrases_parts=questions,
+    #         message_texts=answer
+    #     )
+    #     create_intent()
+        
 
 
 
