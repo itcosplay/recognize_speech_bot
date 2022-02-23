7704fb1f19acf519fcd09bf29d4a051f8fcd076a
konstPialov <azure_dream@mail.ru>
2022-02-24 00:54:42 +0300

    Added DialogFlow to VK bot

--- vk_scripts.py
+++ vk_scripts.py
@@ -6,11 +6,22 @@ from environs import Env
 
 from vk_api.longpoll import VkLongPoll, VkEventType
 
+from dilogflow_scripts import detect_intent_texts
+
 
 def echo (event, vk_api):
+    project_id = os.environ['GOOGLE_PROJECT_ID']
+
+    answer = detect_intent_texts (
+        project_id=project_id,
+        session_id=event.user_id,
+        text=event.text,
+        language_code='ru'
+    )
+
     vk_api.messages.send (
         user_id=event.user_id,
-        message=event.text,
+        message=answer,
         random_id=random.randint(1,1000)
     )
 
